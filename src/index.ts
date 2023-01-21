e1ba22656f6ef44382d9972a32beb8df90428241
Manik <manik@pixelconception.com>
2023-01-21 12:57:40 +0530

    Pushing lecture

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let article: {
+  author: string;
+  content: string;
+  title: string;
+  image?: string;
+};
+article = {
+  author: "Stella",
+  content: "Hello World",
+  title: "My First Article",
+};
