c9d2432208f8814752f6df75cee39a2c13a60b1a
Manik <manik@pixelconception.com>
2023-01-21 14:05:12 +0530

    Pushing lecture

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+type Library = {
+  name: string;
+  address: string;
+  numberOfBooks: number;
+};
+
+const library = {
+  name: "New York Library",
+  address: " 24, Some Street, New York",
+  numberOfBooks: 1254,
+  type: "national", // 'national', 'academic', 'public'
+  books: [
+    {
+      title: "Harry Potter",
+      pages: 756,
+      isbn: "9971-5-0210-0",
+    },
+    {
+      title: "Davinci Code",
+      pages: 386,
+      isbn: "9971-5-0210-0",
+    },
+  ],
+  genres: ["fiction", "history", "computers", "poetry"],
+  members: [
+    {
+      name: "John Doe",
+      phone: "+167678978",
+    },
+    {
+      name: "Mark Doe", // Name is mandatory
+      phone: "+167678978", // Phone is mandatory
+      email: "mark@email.com", // Can have additional optional fields
+    },
+  ],
+};
