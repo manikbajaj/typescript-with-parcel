16d57a8c63294f7566eb8b29640c7dd6dc5a8a52
Manik <manik@pixelconception.com>
2023-01-21 14:22:42 +0530

    Pushing lecture

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+type Reservation = {
+  departureDate: Date;
+  returnDate?: Date;
+  departingFrom: string;
+  destination: string;
+};
+
+type Reserve = {
+  (
+    departureDate: Date,
+    returnDate: Date,
+    departingFrom: string,
+    destination: string
+  ): Reservation | never;
+  (departureDate: Date, departingFrom: string, destination: string):
+    | Reservation
+    | never;
+};
+
+const reserve: Reserve = (
+  departureDate: Date,
+  returnDateOrDepartingFrom: Date | string,
+  departingFromOrDestination: string,
+  destination?: string
+) => {
+  if (returnDateOrDepartingFrom instanceof Date && destination) {
+    return {
+      departureDate: departureDate,
+      returnDate: returnDateOrDepartingFrom,
+      departingFrom: departingFromOrDestination,
+      destination: destination,
+    };
+  } else if (typeof returnDateOrDepartingFrom === "string") {
+    return {
+      departureDate: departureDate,
+      departingFrom: returnDateOrDepartingFrom,
+      destination: departingFromOrDestination,
+    };
+  }
+  throw new Error("Please provide valid details to reserve a ticket");
+};
+
+console.log(reserve(new Date(), new Date(), "New York", "Washington"));
+console.log(reserve(new Date(), "New York", "Washington"));
