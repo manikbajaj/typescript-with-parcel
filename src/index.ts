87f19fa94c06a5624076de5e1247c3d42ca935d9
Manik <manik@pixelconception.com>
2023-02-17 11:54:01 +0530

    Pushing lecture

---
+++ src/index.ts
@@ -0,0 +1,66 @@
+type Holidays = {
+  date: Date;
+  reason: string;
+}[];
+
+abstract class Department {
+  protected abstract holidays: Holidays;
+
+  protected constructor(protected name: string) {}
+
+  public addHolidays(holidays: Holidays) {
+    if (Array.isArray(holidays)) {
+      for (const holiday of holidays) {
+        this.holidays.push(holiday);
+      }
+    }
+  }
+}
+
+class ItDepartment extends Department {
+  protected holidays: Holidays = [];
+
+  constructor() {
+    super("IT Department");
+  }
+}
+
+class AdminDepartment extends Department {
+  protected holidays: Holidays = [];
+
+  constructor() {
+    super("Admin Department");
+  }
+}
+
+const itHolidays: Holidays = [
+  {
+    date: new Date(2022, 11, 1),
+    reason: "IT Department Day",
+  },
+  {
+    date: new Date(2022, 12, 25),
+    reason: "Chistmas",
+  },
+];
+
+const adminHolidays: Holidays = [
+  {
+    date: new Date(2022, 10, 1),
+    reason: "Admin Department Day",
+  },
+  {
+    date: new Date(2022, 12, 25),
+    reason: "Chistmas",
+  },
+];
+
+const itDepartment: ItDepartment = new ItDepartment();
+const adminDepartment: AdminDepartment =
+  new AdminDepartment();
+
+itDepartment.addHolidays(itHolidays);
+adminDepartment.addHolidays(adminHolidays);
+
+console.log(itDepartment);
+console.log(adminDepartment);
