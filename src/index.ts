a57fe5e3a6713c418176e4984734af6ea059af8f
Manik <manik@pixelconception.com>
2023-02-17 11:47:27 +0530

    Pushing lecture

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+type Holidays = {
+  date: Date;
+  reason: string;
+}[];
+
+abstract class Department {
+  protected abstract holidays: Holidays;
+
+  protected constructor(protected name: string) {
+  }
+
+  public addHolidays(holidays: Holidays) {
+    if (Array.isArray(holidays)) {
+      for (const holiday of holidays) {
+        this.holidays.push(holiday);
+      }
+    }
+  }
+}
+
+class ItDepartment extends Department {
+  protected holidays: Holidays = [];
+
+  constructor() {
+    super("IT Department");
+  }
+}
+
+class AdminDepartment extends Department {
+  protected holidays: Holidays = [];
+
+  constructor() {
+    super("Admin Department");
+  }
+}
