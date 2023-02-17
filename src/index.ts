91f58e7ec9175420b9736ed274ae3645305d888d
Manik <manik@pixelconception.com>
2023-02-17 16:42:25 +0530

    Pushing lecture

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+abstract class Person {
+  public abstract name: string;
+  public abstract email: string;
+  public abstract phone: number;
+
+  public greeting() {
+    console.log(`Hello ${this.name}`);
+  }
+}
+
+class RegisteredPerson extends Person {
+  constructor(
+    public name: string,
+    public email: string,
+    public phone: number
+  ) {
+    super();
+  }
+}
+
+const person: RegisteredPerson = new RegisteredPerson(
+  "John",
+  "john@email.com",
+  987889788);
+
+person.greeting();
+console.log(person)
---
+++ src/index2.ts
@@ -0,0 +1,7 @@
+interface User {
+  name: string,
+  email: string,
+  phone: string
+}
+
+class RegisteredUser implements User {}
