709c63e770cc1550ec54175e52fb6b61fa7178ae
Manik <manik@pixelconception.com>
2023-02-17 12:32:53 +0530

    Pushing lecture

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+enum AutomobileTypes {
+  car = "car",
+  bus = "bus",
+  van = "van",
+  truck = "truck",
+  bike = "bike",
+}
+
+enum AutomobileBrands {
+  ferrari = "ferrari",
+  honda = "honda",
+  bmw = "bmw",
+  toyota = "toyota",
+}
+
+enum AutomobileColors {
+  red = "red",
+  blue = "blue",
+  white = "white",
+  black = "black",
+  silver = "silver",
+}
+
+interface Automobile<Type, Brand, Colors> {
+  type: Type;
+  brand: Brand;
+  colors: Colors[];
+  description: string;
+}
+
+class Car
+  implements Automobile<string, AutomobileBrands, AutomobileColors> {
+  public type: string = "car";
+
+  constructor(
+    public brand: AutomobileBrands,
+    public colors: AutomobileColors[],
+    public description: string
+  ) {
+  }
+}
+
+const ferrari: Car = new Car(
+  AutomobileBrands.ferrari,
+  [AutomobileColors.red, AutomobileColors.black],
+  "This is a Ferrari"
+);
+
+console.log(ferrari);
